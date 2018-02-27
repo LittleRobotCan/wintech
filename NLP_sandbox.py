a8ed0f536428901bca3429c2039a0ac6907baef2
LittleRobotCan <ruiwei06@gmail.com>
2018-02-27 07:25:43 -0800

    porterstemmer tested

---
+++ NLP_sandbox.py
@@ -0,0 +1,9 @@
+from nltk.stem import PorterStemmer
+from nltk.tokenize import word_tokenize
+
+ps= PorterStemmer()
+
+words  = ['supported', 'supporting', 'supports']
+
+for word in words:
+    print ps.stem(word)
